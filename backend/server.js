2c6759232c7af8176937536d388e10759566248e
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-18 16:41:42 +0530

    feat(server.js) : updated the port

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log('❌ MongoDB connection error:', err));
 
 // Start the Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server up & running on port: ${PORT}`));
