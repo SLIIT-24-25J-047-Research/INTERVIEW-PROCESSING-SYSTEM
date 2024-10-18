e348fd817fce0b2c1c071f6a2f21687bf69d3eae
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-18 16:31:44 +0530

    feat(serverjs) : updated
    port number changed

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,19 @@ app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/jobs', require('./routes/employer/JobsRoutes')); // Added job routes
 
+// voice confidence routes
+app.use('/api', require('./routes/voiceConfidenceRoutes'));
+//test
+app.get('/api/test', (req, res) => {
+  res.status(200).json({ message: "Node.js backend is running" });
+});
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ MongoDB connected Successfully'))
   .catch(err => console.log('❌ MongoDB connection error:', err));
 
 // Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server up & running on port: ${PORT}`));
