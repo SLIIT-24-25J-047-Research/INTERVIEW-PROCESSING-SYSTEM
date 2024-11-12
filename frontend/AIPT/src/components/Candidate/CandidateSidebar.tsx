0e370e054fb20d37c78b9842f87de1c878d21f11
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-12 17:34:52 +0530

    Update CandidateSidebar.tsx

--- frontend/AIPT/src/components/Candidate/CandidateSidebar.tsx
+++ frontend/AIPT/src/components/Candidate/CandidateSidebar.tsx
@@ -1,32 +1,40 @@
-import React from 'react';
-import { FaHome, FaTasks, FaUserAlt, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaHome, FaTasks, FaUserAlt, FaCog, FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import './candidateSidebar.css'; // Make sure to create this CSS file
+import './candidateSidebar.css';
 
 const CandidateSidebar: React.FC = () => {
-  return (
-    <div className="sidebar">
-      <ul className="menu-list">
-        <li className="menu-item">
-          <FaHome className="menu-icon" />
-          <Link to="/candidate-home">Dashboard</Link>
-        </li>
-        <li className="menu-item">
-          <FaTasks className="menu-icon" />
-          <Link to="/assignments">Assignments</Link>
-        </li>
-        <li className="menu-item">
-          <FaUserAlt className="menu-icon" />
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li className="menu-item">
-          <FaCog className="menu-icon" />
-          <Link to="/settings">Settings</Link>
-        </li>
-       
-      </ul>
-    </div>
-  );
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    return (
+        <div
+            className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}
+            onMouseEnter={() => setIsCollapsed(false)}
+            onMouseLeave={() => setIsCollapsed(true)}
+        >
+            {/* <div className="hamburger-icon" onClick={() => setIsCollapsed(!isCollapsed)}>
+                <FaBars />
+            </div> */}
+            <ul className="menu-list">
+                <li className="menu-item">
+                    <FaHome className="menu-icon" />
+                    {!isCollapsed && <Link to="/candidate-home">Dashboard</Link>}
+                </li>
+                <li className="menu-item">
+                    <FaTasks className="menu-icon" />
+                    {!isCollapsed && <Link to="/assignments">Assignments</Link>}
+                </li>
+                <li className="menu-item">
+                    <FaUserAlt className="menu-icon" />
+                    {!isCollapsed && <Link to="/profile">Profile</Link>}
+                </li>
+                <li className="menu-item">
+                    <FaCog className="menu-icon" />
+                    {!isCollapsed && <Link to="/settings">Settings</Link>}
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default CandidateSidebar;
