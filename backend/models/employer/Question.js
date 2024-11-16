51a25ff5494dd00eed12f53fd607a8ab2b8ad4b7
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-16 08:19:14 +0530

    Update Question.js

--- backend/models/employer/Question.js
+++ backend/models/employer/Question.js
@@ -8,6 +8,10 @@ const questionSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    answers: {
+        type: [String], 
+        required: true, 
+    },
     createdAt: {
         type: Date,
         default: Date.now
