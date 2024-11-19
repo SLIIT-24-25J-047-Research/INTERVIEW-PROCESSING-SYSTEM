b0dd4f49056a0ad47c45db2fbe543da030734672
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-19 13:56:34 +0530

    Update voiceConfidenceRoutes.js

--- backend/routes/voiceConfidenceRoutes.js
+++ backend/routes/voiceConfidenceRoutes.js
@@ -4,19 +4,17 @@ const path = require('path');
 const { predictConfidence } = require('../controllers/predictController');
 
 const router = express.Router();
-// Set up multer for file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads'); // Specify the uploads folder
+        cb(null, 'uploads'); 
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Rename the file
+        cb(null, Date.now() + path.extname(file.originalname)); 
     }
 });
 
 const upload = multer({ storage: storage });
 
-// Define the route for audio and text prediction
 router.post('/predict', upload.single('audio'), predictConfidence);
 
 
