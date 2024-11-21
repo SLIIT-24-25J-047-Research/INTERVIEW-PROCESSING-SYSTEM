996f36276e2ab1b57b2810f2f539ac8fe2ab2de4
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-21 16:51:18 +0530

    Update SkillGroup.js

--- backend/models/employer/SkillGroup.js
+++ backend/models/employer/SkillGroup.js
@@ -4,7 +4,7 @@ const skillGroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   skills: { type: [String], required: true }, 
   focus: { type: String, enum: ['frontend', 'backend', 'fullstack'], required: true }, 
-  groupId: { type: String, required: true, unique: true },  // Unique identifier for the group
+  groupId: { type: String, required: true, unique: true },  
 });
 
 module.exports = mongoose.model('SkillGroup', skillGroupSchema);
