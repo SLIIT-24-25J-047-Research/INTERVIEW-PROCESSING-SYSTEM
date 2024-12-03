fdeec66f001a4cfa4e923f0e7dba6e96b65d4c5f
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-12-03 16:06:17 +0530

    chore(app) : changed port to 3001

--- backend/stress_detection_service/app.py
+++ backend/stress_detection_service/app.py
@@ -115,4 +115,4 @@ def capture_and_predict():
     return jsonify({'stress_level': stress_level, 'emotion': emotion})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3001)
