2d6b6b77cc5804171b895b39953f2d4b7c37a1e1
IT21319792 <IT21319792@my.sliit.lk>
2024-12-27 16:25:26 +0530

    Create NonTechInterviewRoutes.js

---
+++ backend/routes/employer/NonTechInterviewRoutes.js
@@ -0,0 +1,14 @@
+// routes/interviewRoutes.js
+const express = require('express');
+const { scheduleInterview, editInterview, cancelInterview, getAllSchedules, getScheduleById, getSchedulesByUserId, } = require('../../controllers/employer/nonTechInterviewController');
+const router = express.Router();
+
+// Route to schedule an interview
+router.post('/schedule', scheduleInterview);
+router.put('/schedule/edit/:id', editInterview);
+router.delete('/cancel/:id', cancelInterview);
+router.get('/schedule/get', getAllSchedules);
+router.get('/schedule/get/:id', getScheduleById);
+router.get('/user/:userId', getSchedulesByUserId);
+
+module.exports = router;
