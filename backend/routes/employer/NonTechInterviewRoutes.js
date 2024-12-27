4eb6e70203d5040ef1353b1f2278b67d944264b6
IT21319792 <IT21319792@my.sliit.lk>
2024-12-27 16:30:27 +0530

    Update NonTechInterviewRoutes.js

--- backend/routes/employer/NonTechInterviewRoutes.js
+++ backend/routes/employer/NonTechInterviewRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/NT-schedule', scheduleInterview);
 router.put('/NT-schedule/edit/:id', editInterview);
 router.delete('/NT-cancel/:id', cancelInterview);
-router.get('/schedule/get', getAllSchedules);
+router.get('/NT-schedule/get', getAllSchedules);
 router.get('/schedule/get/:id', getScheduleById);
 router.get('/user/:userId', getSchedulesByUserId);
 
