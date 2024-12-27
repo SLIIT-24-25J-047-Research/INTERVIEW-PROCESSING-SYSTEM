489ae93a607f86989438f08cf817352579bffabf
IT21319792 <IT21319792@my.sliit.lk>
2024-12-27 16:26:29 +0530

    Update TechInterviewRoutes.js

--- backend/routes/employer/TechInterviewRoutes.js
+++ backend/routes/employer/TechInterviewRoutes.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const technicalInterviewController = require('../controllers/technicalInterviewController');
+const technicalInterviewController = require('../../controllers/employer/TechInterviewController');
 
-// Create a new technical interview schedule
+
 router.post('/schedule', technicalInterviewController.createTechnicalInterview);
-
-// Get all technical interview schedules
 router.get('/schedules', technicalInterviewController.getAllTechnicalInterviews);
-
-// Get a specific technical interview schedule by ID
 router.get('/schedule/:id', technicalInterviewController.getTechnicalInterviewById);
-
-// Update a technical interview schedule
 router.put('/schedule/:id', technicalInterviewController.updateTechnicalInterview);
-
-// Delete a technical interview schedule
 router.delete('/schedule/:id', technicalInterviewController.deleteTechnicalInterview);
 
 module.exports = router;
