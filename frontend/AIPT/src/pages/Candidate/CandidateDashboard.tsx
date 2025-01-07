0fcec9dd43f6a19e3613a10c5ffed9873a312a6f
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-07 11:58:55 +0530

    Update CandidateDashboard.tsx

--- frontend/AIPT/src/pages/Candidate/CandidateDashboard.tsx
+++ frontend/AIPT/src/pages/Candidate/CandidateDashboard.tsx
@@ -188,6 +188,7 @@ const CandidateDashboard: React.FC = () => {
         `}
       </style>
       <CandidateLayout>
+        {/* <CandidateSidebar /> */}
         <div className="dashboard">
           <h1 className="welcome">Welcome, {name}</h1>
           <div className="box-container">
