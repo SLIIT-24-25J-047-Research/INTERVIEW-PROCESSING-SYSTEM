087f03e4939c0ccf8543d67201178de1f7dc2d13
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-07 11:58:52 +0530

    Update CandidateTest.tsx

--- frontend/AIPT/src/pages/Candidate/CandidateTest.tsx
+++ frontend/AIPT/src/pages/Candidate/CandidateTest.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "../../components/Candidate/CandidateSidebar";
 import Header from "../../components/Candidate/CandidateHeader";
-import "../../components/Candidate/candidateLayout.css";
+import "../../components/Candidate/css/candidateLayout.css";
 import CodeEditor from "../../components/Candidate/CodeEditor";
 
 const CandidateTest: React.FC = () => {
