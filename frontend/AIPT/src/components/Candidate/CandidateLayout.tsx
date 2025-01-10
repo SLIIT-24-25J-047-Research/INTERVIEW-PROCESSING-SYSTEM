0c9e521ab557647ded90cfb5459fd3683425621b
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-10 12:12:38 +0530

    Update CandidateLayout.tsx

--- frontend/AIPT/src/components/Candidate/CandidateLayout.tsx
+++ frontend/AIPT/src/components/Candidate/CandidateLayout.tsx
@@ -6,7 +6,7 @@ const CandidateLayout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     return (
         <div className="flex flex-row h-full overflow-hidden ">
             <CandidateSidebar />
-            <main className="flex-grow flex flex-col mt-16  bg-[#f9f9f9] overflow-y-auto transition-all duration-300 ease-in-out">
+            <main className="flex-grow flex flex-col mt-2  bg-[#f9f9f9] overflow-y-auto transition-all duration-300 ease-in-out">
                 <CandidateHeader title="Dashboard" />
                 <div className="flex-grow overflow-x-auto overflow-y-auto p-20 flex flex-col gap-5 text-center my-5">
                     {children}
