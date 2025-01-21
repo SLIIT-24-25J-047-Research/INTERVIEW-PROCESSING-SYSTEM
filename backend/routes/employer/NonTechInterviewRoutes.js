1b01a1768885e1bb9e382120dbb80c6abf4f1acb
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-21 15:59:38 +0530

    Update NonTechInterviewRoutes.js

--- backend/routes/employer/NonTechInterviewRoutes.js
+++ backend/routes/employer/NonTechInterviewRoutes.js
@@ -9,6 +9,6 @@ router.put('/schedule/edit/:id', editInterview);
 router.delete('/cancel/:id', cancelInterview);
 router.get('/schedule/get', getAllSchedules);
 router.get('/schedule/get/:id', getScheduleById);
-router.get('/shedule/user/:userId', getSchedulesByUserId);
+router.get('/schedule/user/:userId', getSchedulesByUserId);
 
 module.exports = router;
