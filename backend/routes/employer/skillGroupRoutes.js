d8d2724fce0b97f7b8efe8be18e69c0afb11f8b3
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-25 16:32:24 +0530

    Update skillGroupRoutes.js

--- backend/routes/employer/skillGroupRoutes.js
+++ backend/routes/employer/skillGroupRoutes.js
@@ -3,10 +3,11 @@ const skillGroupController = require('../../controllers/employer/skillGroupContr
 
 const router = express.Router();
 
-router.get('/skills', skillGroupController.getSkillGroupsBySkills);  
 
 router.post('/', skillGroupController.createSkillGroup);
 router.get('/', skillGroupController.getAllSkillGroups);
+
+router.get('/skills', skillGroupController.getSkillGroupsBySkills);  
 router.get('/:id', skillGroupController.getSkillGroupById);
 router.put('/:id', skillGroupController.updateSkillGroup);
 router.delete('/:id', skillGroupController.deleteSkillGroup);
