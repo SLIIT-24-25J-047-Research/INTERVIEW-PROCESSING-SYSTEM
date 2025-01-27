0abb36c4b23a7335bbf4dfdb4d8c1398e881e20b
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-27 12:54:08 +0530

    Update Header.tsx

--- frontend/AIPT/src/components/Interviewer/Header.tsx
+++ frontend/AIPT/src/components/Interviewer/Header.tsx
@@ -16,8 +16,8 @@ function logout() {
 const Header: React.FC = () => {
   return (
     <header className="bg-white shadow-sm">
-    <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-gray-900">Recruiter Dashboard</h1>
+    <div className="max-w-full mx-auto px-4 py-4 sm:px-6 lg:px-8 flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-gray-900">Admin Dashboard</h1>
       <div className="flex items-center space-x-4">
         <button className="p-2 rounded-full hover:bg-gray-100">
           <Search className="h-5 w-5 text-gray-500" />
