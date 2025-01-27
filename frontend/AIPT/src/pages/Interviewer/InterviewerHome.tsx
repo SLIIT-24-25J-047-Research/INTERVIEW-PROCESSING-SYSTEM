2c3b2dbecafe43002afa00afce61d52919c99ccc
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-27 12:54:12 +0530

    Update InterviewerHome.tsx

--- frontend/AIPT/src/pages/Interviewer/InterviewerHome.tsx
+++ frontend/AIPT/src/pages/Interviewer/InterviewerHome.tsx
@@ -35,7 +35,7 @@ function InterviewerHome() {
           <div className="min-h-screen bg-gray-50 mt-10">
     
 
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Stats */}
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
           <div className="bg-white overflow-hidden shadow rounded-lg">
