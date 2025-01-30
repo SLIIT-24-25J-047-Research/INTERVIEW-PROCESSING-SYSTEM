eebc96b82cce88e91e9af1f118db152d48788bca
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-30 17:34:05 +0530

    Update InterviewStore.tsx

--- frontend/AIPT/src/components/store/InterviewStore.tsx
+++ frontend/AIPT/src/components/store/InterviewStore.tsx
@@ -5,20 +5,24 @@ interface InterviewState {
   currentQuestionIndex: number;
   questions: Question[];
   score: number;
-  timeRemaining: number;
+  timeRemaining: number | null;
   answers: Record<number, any>;
+  lockedQuestions: Set<number>;
   setCurrentQuestion: (index: number) => void;
   setAnswer: (questionId: number, answer: any) => void;
   updateScore: (points: number) => void;
-  updateTimeRemaining: (value: number | ((prev: number) => number)) => void;
+  updateTimeRemaining: (time: number | ((prev: number) => number)) => void;
+  lockQuestion: (questionId: number) => void;
+  isQuestionLocked: (questionId: number) => boolean;
 }
 
-export const useInterviewStore = create<InterviewState>((set) => ({
+export const useInterviewStore = create<InterviewState>((set, get) => ({
   currentQuestionIndex: 0,
   questions: [],
   score: 0,
-  timeRemaining: 0,
+  timeRemaining: null,
   answers: {},
+  lockedQuestions: new Set(),
   setCurrentQuestion: (index) => set({ currentQuestionIndex: index }),
   setAnswer: (questionId, answer) =>
     set((state) => ({
@@ -28,6 +32,12 @@ export const useInterviewStore = create<InterviewState>((set) => ({
     set((state) => ({ score: state.score + points })),
   updateTimeRemaining: (time) =>
     set((state) => ({
-      timeRemaining: typeof time === 'function' ? time(state.timeRemaining) : time,
+      timeRemaining: typeof time === 'function' ? time(state.timeRemaining ?? 0) : time,
     })),
+  lockQuestion: (questionId) => 
+    set((state) => ({
+      lockedQuestions: new Set([...state.lockedQuestions, questionId])
+    })),
+  isQuestionLocked: (questionId) => 
+    get().lockedQuestions.has(questionId),
 }));
