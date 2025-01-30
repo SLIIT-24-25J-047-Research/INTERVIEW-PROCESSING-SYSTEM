d9b43f97b2a9fb2d42ac0c13a957d84eb53afa99
BoshithaMGunarathna <IT21319792@my.sliit.lk>
2025-01-30 17:00:08 +0530

    Update CandidateTest.tsx

--- frontend/AIPT/src/pages/Candidate/candidate-dashboard/CandidateTest.tsx
+++ frontend/AIPT/src/pages/Candidate/candidate-dashboard/CandidateTest.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Sidebar from "../../../components/Candidate/CandidateSidebar";
 import Header from "../../../components/Candidate/CandidateHeader";
-import CodeEditor from "../../../components/Candidate/CodeEditor";
+import { CodeEditor } from "../../../components/Candidate/CodeEditor";
 import { Timer } from "lucide-react";
 
 interface InterviewState {
@@ -294,12 +294,12 @@ const CandidateTest: React.FC = () => {
             <div className="flex-1 flex flex-col gap-4">
               <p className="text-lg font-medium">{questions[currentQuestionIndex]}</p>
               <div className="relative w-full h-[60vh] border rounded shadow bg-gray-800">
-                <CodeEditor
+                {/* <CodeEditor
                   key={currentQuestionIndex}
-                  onCodeChange={handleCodeChange}
-                  initialCode={answers[currentQuestionIndex]}
+                  onChange={handleCodeChange}
+                  value={answers[currentQuestionIndex]}
                   readOnly={isTestEnded}
-                />
+                /> */}
               </div>
             </div>
           </div>
