b969db3db351b9f4f0734cb3e93cc6570c76b873
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-03 14:07:18 +0530

    Update App.tsx

--- frontend/AIPT/src/App.tsx
+++ frontend/AIPT/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import InterviewerHome from "./pages/Interviewer/InterviewerHome";
