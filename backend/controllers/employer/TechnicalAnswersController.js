4c33b201638db095d3d897aa9bd6456d67bd3af9
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-06 13:51:42 +0530

    Create TechnicalAnswersController.js

---
+++ backend/controllers/employer/TechnicalAnswersController.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const TecInterviewAnswerSchema = new mongoose.Schema({
+  interviewId: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  answers: [
+    {
+      questionId: { type: String, required: true },
+      response: { type: mongoose.Schema.Types.Mixed, required: true },
+      timeTaken: { type: Number, required: true }, 
+    },
+  ],
+  submittedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('technicalAnswers', TecInterviewAnswerSchema);
