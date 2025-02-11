48edcbda8a1c245a12543d99c0bd6cab0920c9a5
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-11 15:23:01 +0530

    Update AnswersDashboaard.tsx

--- frontend/AIPT/src/components/Interviewer/answers/AnswersDashboaard.tsx
+++ frontend/AIPT/src/components/Interviewer/answers/AnswersDashboaard.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { AnswersList } from './AnswersList';
 import { AnswerDetails } from './AnswerDetails';
+import DashboardLayout from '../DashboardLayout';
 
 export const AnswersDashboard: React.FC = () => {
   const [selectedSubmission, setSelectedSubmission] = useState<string | null>(null);
 
   return (
+    <DashboardLayout>
     <div>
       {selectedSubmission ? (
         <AnswerDetails
@@ -16,5 +18,6 @@ export const AnswersDashboard: React.FC = () => {
         <AnswersList onSelectInterview={setSelectedSubmission} />
       )}
     </div>
+    </DashboardLayout>
   );
 };
