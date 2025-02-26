02f50fa5244af135e1b7188d0f76c3b445a7b7d6
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-26 14:14:46 +0530

    Update CandidateSidebar.tsx

--- frontend/AIPT/src/components/Candidate/CandidateSidebar.tsx
+++ frontend/AIPT/src/components/Candidate/CandidateSidebar.tsx
@@ -14,8 +14,8 @@ const menuItems: MenuItem[] = [
   { path: '/dashboard', icon: Home, label: 'Dashboard' },
   { path: '/candidate-mockup', icon: PenTool, label: 'Mockup-Test' },
   { path: '/interview', icon: Users, label: ' Interview' },
-  { path: '/non-tech-interview', icon: Users, label: 'Non Technical Interview' },
-  { path: '/tech-interview', icon: Code, label: 'Technical Interview' },
+  // { path: '/non-tech-interview', icon: Users, label: 'Non Technical Interview' },
+  // { path: '/tech-interview', icon: Code, label: 'Technical Interview' },
   { path: '/profile', icon: User, label: 'Profile' },
 ];
 
