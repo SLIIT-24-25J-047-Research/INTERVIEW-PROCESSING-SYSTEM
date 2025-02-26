685a2f2cbfb449af87bb8e4c0d077fa38cead81e
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-26 17:12:52 +0530

    Update Sidebar.tsx

--- frontend/AIPT/src/components/Interviewer/Sidebar.tsx
+++ frontend/AIPT/src/components/Interviewer/Sidebar.tsx
@@ -14,7 +14,7 @@ interface MenuItem {
 const menuItems: MenuItem[] = [
   { path: '/interviewer-home', icon: Home, label: 'Dashboard' },
   { path: '/interviewer-profile', icon: PenTool, label: 'profile' },
-  { path: '/interviewer-interviews', icon: Users, label: ' Interview' },
+  { path: '/interviewer-feedback', icon: Users, label: ' Feedbacks' },
   { path: '/interviewer-Vacancies', icon: Users, label: 'Non Technical Interview' },
 
 ];
