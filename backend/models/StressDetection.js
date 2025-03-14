48f352ac179d97cce01035d4a488c45613cb7f14
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-03-14 13:03:43 +0530

    Update StressDetection.js

--- backend/models/StressDetection.js
+++ backend/models/StressDetection.js
@@ -1,21 +1,6 @@
 const mongoose = require('mongoose');
 
-const stressDetectionSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    interviewScheduleId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Technical-InterviewSchedule',
-        required: true
-    },
-    jobId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'jobs',
-        required: true
-    },
+const questionSchema = new mongoose.Schema({
     questionId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'TechnicalQuestion',
@@ -32,7 +17,26 @@ const stressDetectionSchema = new mongoose.Schema({
     stressLevel: {
         type: String,
         required: true
+    }
+});
+
+const stressDetectionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    interviewScheduleId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Technical-InterviewSchedule',
+        required: true
+    },
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'jobs',
+        required: true
+    },
+    questions: [questionSchema], 
     createdAt: {
         type: Date,
         default: Date.now
